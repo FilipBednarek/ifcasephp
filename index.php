b112f803c9a20c14218fe0bd8a7b0728f326fca4
FilipBednarek <131236640+FilipBednarek@users.noreply.github.com>
2023-10-09 10:22:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,18 +8,23 @@ else if ($temp >= 36 and $temp <= 36.6){
     echo('stan podgoraczkowy');
 } elseif ($temp >= 37 and $temp <= 41){
     echo("goraczka23");
-}
-*/
+}*/
 
-$temp = 36.6;
+// $temp = 36.6;
 
-switch ($temp) {
-    case ($temp <= 36 or $temp >= 41): echo "alert"; break;
-    case ($temp >= 36 and $temp <= 36.6): echo "ok"; break;
-    case ($temp  >= 36.6 and $temp <= 37): echo "stan"; break;
-    case ($temp >= 37 and $temp <= 41): echo 'goraczka'; break;
+// switch ($temp) {
+//     case ($temp <= 36 or $temp >= 41): echo "alert"; break;
+//     case ($temp >= 36 and $temp <= 36.6): echo "ok"; break;
+//     case ($temp  >= 36.6 and $temp <= 37): echo "stan"; break;
+//     case ($temp >= 37 and $temp <= 41): echo 'goraczka'; break;
+// }
 
+// define ("stala", 152.122);
+// echo stala;
 
+// $a = 12;
+// $b = 1;
 
-}
+// echo $a !== $b;
+
 ?>
