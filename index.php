50fd100027cf6863162399289e78103e2c11ce26
FilipBednarek <131236640+FilipBednarek@users.noreply.github.com>
2023-10-09 09:51:06 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+/*if ($temp <= 36 or $temp >= 41){
+    echo('alert');
+}
+else if ($temp >= 36 and $temp <= 36.6){
+    echo("ok");
+} else if ($temp  >= 36.6 and $temp <= 37){
+    echo('stan podgoraczkowy');
+} elseif ($temp >= 37 and $temp <= 41){
+    echo("goraczka23");
+}
+*/
+
+$temp = 36.6;
+
+switch ($temp) {
+    case ($temp <= 36 or $temp >= 41): echo "alert"; break;
+    case ($temp >= 36 and $temp <= 36.6): echo "ok"; break;
+    case ($temp  >= 36.6 and $temp <= 37): echo "stan"; break;
+    case ($temp >= 37 and $temp <= 41): echo 'goraczka'; break;
+
+
+
+}
+?>
